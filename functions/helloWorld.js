6e520d13a38f498178ad3e113213f2b9db6b039f
Travis Richardson <tlrichar@gmail.com>
2023-04-30 13:59:51 -0400

    Added user entry

--- functions/helloWorld.js
+++ functions/helloWorld.js
@@ -1,3 +1,4 @@
+/*
 exports.handler = async function(event, context) {
     return {
         statusCode: 200,
@@ -5,4 +6,31 @@ exports.handler = async function(event, context) {
             message: 'Hello from Lambda!'
         })
     }
-}
+}*/
+exports.handler = async function(event, context) {
+    if (event.httpMethod !== "POST") {
+        return {
+            statusCode: 405,
+            body: JSON.stringify({ error: "Method Not Allowed" })
+        };
+    }
+
+    try {
+        const requestBody = JSON.parse(event.body);
+        const message = requestBody.message;
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                message: `Your message is: ${message}!`
+            })
+        };
+    } catch (error) {
+        console.error(error);
+
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: "Internal Server Error" })
+        };
+    }
+};
--- src/App.js
+++ src/App.js
@@ -1,35 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
+
 function App() {
+  const [message, setMessage] = useState('');
 
-  const [message, setMessage] = useState('text');
-  const fetchData = async () => {
-    const results = await axios.get('.netlify/functions/helloWorld')
-    console.log(results.data.message)
-    setMessage(results.data.message)
-  }
+  const submitForm = async (event) => {
+    event.preventDefault();
 
-  useEffect(() => {
-    fetchData()
-  },[])
+    const messageInput = document.getElementById("message-input");
+    const message = messageInput.value;
+
+    setMessage(''); // clear the message out
+
+    try {
+      const response = await axios.post('/.netlify/functions/helloWorld', { message });
+      setMessage(response.data.message);
+    } catch (error) {
+      console.log(error.response.data);
+      setMessage("Error: " + error.response.data.error);
+    }
+  };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{message}</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p className="message">{message}</p>
+          <form onSubmit={submitForm}>
+            <label htmlFor="message-input">Enter a message:</label>
+            <input type="text" id="message-input" name="message" />
+            <button type="submit">Submit</button>
+          </form>
+          <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+          >
+
+          </a>
+        </header>
+      </div>
   );
 }
 
